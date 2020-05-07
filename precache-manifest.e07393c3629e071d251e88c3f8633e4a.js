44c2bd60894cc6b6cec6833cc6edc2a862a2a963
niikitabhatt <nikitabhatt099@gmail.com>
2020-05-07 02:30:36 -0700

    Updates

---
+++ precache-manifest.e07393c3629e071d251e88c3f8633e4a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "edb86bf298a15e928e9e2bed928a1bae",
+    "url": "/react-hands-on-todolist/index.html"
+  },
+  {
+    "revision": "9dd563eb81f8446a2836",
+    "url": "/react-hands-on-todolist/static/css/main.7ba19002.chunk.css"
+  },
+  {
+    "revision": "26e588240e6a7c6e4a3e",
+    "url": "/react-hands-on-todolist/static/js/2.e2e02c08.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-hands-on-todolist/static/js/2.e2e02c08.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9dd563eb81f8446a2836",
+    "url": "/react-hands-on-todolist/static/js/main.7b53cdbc.chunk.js"
+  },
+  {
+    "revision": "99d41ed020fd8857bb5c",
+    "url": "/react-hands-on-todolist/static/js/runtime-main.adc42687.js"
+  }
+]);
